1949acc432f1506b5956c54eb312f1ad833fa284
Jay Robinson <me@j4y.co>
2016-01-11 20:03:39 +0000

    Refactor entry

--- app/src/Entry.php
+++ app/src/Entry.php
@@ -15,29 +15,42 @@ class Entry implements \JsonSerializable
 
     public function __construct($config)
     {
-        if ($config['h'] !== 'entry') {
-            throw new RuntimeException($config['h'] . ' is not a valid "h"');
-        }
+        $this->validateH($config);
+        $config['published'] = $this->validateDate($config);
+        $this->checkEntryHasData($config);
+
         unset($config['h']);
-
-        try {
-            $config['published'] = (isset($config['published']))
-                ? new DateTimeImmutable($config['published'])
-                : new DateTimeImmutable();
-            $config['published'] = $config['published']->format("Y-m-d H:i:s");
-        } catch (\Exception $e) {
-            throw new RuntimeException($config['published'] . ' is not a valid date');
-        }
-
-        if (!isset($config['content']) && !isset($config['photo'])) {
-            throw new RuntimeException('content or photo have to be set');
-        }
-
         $this->properties = $config;
     }
 
     public function jsonSerialize()
     {
         return $this->properties;
+    }
+
+    private function validateH($config)
+    {
+        if ($config['h'] !== 'entry') {
+            throw new RuntimeException($config['h'] . ' is not a valid "h"');
+        }
+    }
+
+    private function validateDate($config)
+    {
+        try {
+            $published = (isset($config['published']))
+                ? new DateTimeImmutable($config['published'])
+                : new DateTimeImmutable();
+            return $published->format("Y-m-d H:i:s");
+        } catch (\Exception $e) {
+            throw new RuntimeException($config['published'] . ' is not a valid date');
+        }
+    }
+
+    private function checkEntryHasData($config)
+    {
+        if (!isset($config['content']) && !isset($config['photo'])) {
+            throw new RuntimeException('content or photo have to be set');
+        }
     }
 }
