3b718e2a8f9da0bdd3c493166087962f62c125df
Jay Robinson <me@j4y.co>
2016-02-19 14:58:08 +0000

    Add repo.findById()

--- app/src/EntryRepository.php
+++ app/src/EntryRepository.php
@@ -16,6 +16,29 @@ class EntryRepository
         $this->filesystem = $filesystem;
     }
 
+    public function findById($post_id)
+    {
+        $post_filepath = array_shift(
+            array_filter(
+                $this->getJsonFilePaths(),
+                function ($file_path) use ($post_id) {
+                    return $file_path['filename'] == $post_id;
+                }
+            )
+        );
+        return $this->filesystem->read($post_filepath['path']);
+    }
+
+    private function getJsonFilePaths()
+    {
+        return array_filter(
+            $this->filesystem->listContents('', true),
+            function ($file_path) {
+                return substr($file_path['path'], -4) == "json";
+            }
+        );
+    }
+
     public function save(Post $entry, $files)
     {
         try {
