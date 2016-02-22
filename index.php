4cffbc7abc76c81e4eaaca76f21e2d040b6b084c
Jay Robinson <me@j4y.co>
2016-02-22 12:28:59 +0000

    Install Dotenv

--- index.php
+++ index.php
@@ -4,11 +4,14 @@ require_once __DIR__.'/vendor/autoload.php';
 
 Symfony\Component\Debug\ErrorHandler::register();
 
+$dotenv = new Dotenv\Dotenv("..");
+$dotenv->load();
+
 $app = new Silex\Application();
 $app['debug'] = true;
 $app['filestore_root'] = "/tmp/aruna";
 
-// SERVICES
+// PROVIDERS
 $app->register(new Silex\Provider\ServiceControllerServiceProvider());
 $app->register(new Silex\Provider\UrlGeneratorServiceProvider());
 $app->register(new Silex\Provider\MonologServiceProvider(), array(
@@ -19,6 +22,7 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/views',
 ));
 
+// SERVICES
 $app['posts_repository'] = $app->share(function () use ($app) {
     $adapter = new League\Flysystem\Adapter\Local($app['filestore_root']);
     $filesystem = new League\Flysystem\Filesystem($adapter);
@@ -30,7 +34,6 @@ $app['create_post.handler'] = $app->share(function () use ($app) {
         new Aruna\ImageResizer()
     );
 });
-
 $app['micropub.controller'] = $app->share(function () use ($app) {
     return new Aruna\Controller\MicropubController(
         $app["monolog"],
