08f915a346691550f3c3a8a44793df39b04898d7
Jay Robinson <me@j4y.co>
2016-03-01 16:09:51 +0000

    Add data-dir perms stuff to bootstrap.sh

--- resources/vagrant_bootstrap.sh
+++ resources/vagrant_bootstrap.sh
@@ -1,4 +1,15 @@
+DATA_DIR="/tmp/aruna"
+
 sudo locale-gen en_GB.UTF-8
+
+## SORT OUT DATA DIR PERMS
+sudo mkdir $DATA_DIR
+sudo usermod -aG www-data vagrant;
+sudo addgroup www-data;
+sudo chgrp -Rv www-data $DATA_DIR;
+sudo chmod -Rv g+w $DATA_DIR;
+
+sudo touch /srv/.env
 
 sudo apt-get update \
     && sudo apt-get install -y \
@@ -9,6 +20,7 @@ sudo apt-get update \
 
 sudo cp /srv/aruna/nginx.conf /etc/nginx/sites-enabled/default \
     && sudo cp /srv/aruna/php5-fpm.conf /etc/php5/fpm/pool.d/aruna.conf
+
 
 sudo service php5-fpm restart
 sudo service nginx restart
