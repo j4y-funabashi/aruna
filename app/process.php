918c84c9bf27ccabdd324268b2a26a271eb8d46c
Jay Robinson <me@j4y.co>
2016-03-01 17:04:57 +0000

    Start extracting methods in process.php

--- app/process.php
+++ app/process.php
@@ -1,48 +1,57 @@
 <?php
 
 require_once __DIR__ . "/../common.php";
-$posts_root = "/tmp/aruna/posts";
-$thumbnails_root = "/tmp/aruna/thumbnails";
 
-// GET SORTED LIST OF ALL JSON FILES
-$dir = new CallbackFilterIterator(
-    new RecursiveIteratorIterator(
-        new RecursiveDirectoryIterator($posts_root)
-    ),
-    function ($current, $key, $iterator) {
-        return substr($current, -4) == "json";
+function listJsonFiles($root_dir)
+{
+    $dir = new CallbackFilterIterator(
+        new RecursiveIteratorIterator(
+            new RecursiveDirectoryIterator($root_dir)
+        ),
+        function ($current, $key, $iterator) {
+            return substr($current, -4) == "json";
+        }
+    );
+    $json_files = [];
+    foreach ($dir as $fileInfo) {
+        $json_files[$fileInfo->getRealPath()] = $fileInfo;
     }
-);
-$json_files = [];
-foreach ($dir as $fileInfo) {
-    $json_files[$fileInfo->getRealPath()] = $fileInfo;
+    ksort($json_files);
+    return $json_files;
 }
-ksort($json_files);
 
-
-
-// BUILD PIPELINE
-use League\Pipeline\Pipeline;
-use Aruna\Action\ImageResizer;
-use Aruna\Action\ResizePhoto;
-
-$pipeline = (new Pipeline())
-    ->pipe(new ResizePhoto(new ImageResizer($posts_root, $thumbnails_root)));
-
-// PUSH CONTENTS OF EACH FILE THROUGH PIPELINE
-foreach ($json_files as $fileInfo) {
+function readFileContents($fileInfo)
+{
     // read post array from json file
     $fileObject = $fileInfo->openFile();
     $data = [];
     while (!$fileObject->eof()) {
         $data[] = $fileObject->fgets();
     }
-    $post = json_decode(implode("\n", $data), true);
+    return json_decode(implode("\n", $data), true);
+}
 
+use League\Pipeline\Pipeline;
+use Aruna\Action\ImageResizer;
+use Aruna\Action\ResizePhoto;
+
+$posts_root = "/tmp/aruna/posts";
+$thumbnails_root = "/tmp/aruna/thumbnails";
+
+// BUILD PIPELINE
+$pipeline = (new Pipeline())
+    ->pipe(new ResizePhoto(new ImageResizer($posts_root, $thumbnails_root)));
+
+// PUSH CONTENTS OF EACH FILE THROUGH PIPELINE
+foreach (listJsonFiles($posts_root) as $fileInfo) {
     try {
-        $pipeline->process($post);
+        $pipeline->process(readFileContents($fileInfo));
     } catch (Exception $e) {
-        print PHP_EOL."CRITICAL:";
-        var_dump($e->getMessage());
+        $m = sprintf(
+            "Could not process %s [%s]",
+            $fileInfo->getRealPath(),
+            $e->getMessage()
+        );
+        var_dump($m);
     }
 }
