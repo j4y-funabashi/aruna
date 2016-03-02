435c1bfcd3f0d7074e386837f5677068b1414612
Jay Robinson <me@j4y.co>
2016-03-02 10:23:19 +0000

    Move conf files to resources

--- resources/vagrant_bootstrap.sh
+++ resources/vagrant_bootstrap.sh
@@ -18,8 +18,8 @@ sudo apt-get update \
     php5-cli \
     php5-imagick
 
-sudo cp /srv/aruna/nginx.conf /etc/nginx/sites-enabled/default \
-    && sudo cp /srv/aruna/php5-fpm.conf /etc/php5/fpm/pool.d/aruna.conf
+sudo cp /srv/aruna/resources/nginx.conf /etc/nginx/sites-enabled/default \
+    && sudo cp /srv/aruna/resources/php5-fpm.conf /etc/php5/fpm/pool.d/aruna.conf
 
 
 sudo service php5-fpm restart
