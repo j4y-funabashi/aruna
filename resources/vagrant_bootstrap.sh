a5c8c271192f6642da1ca21d7f298b7958b39db2
Jay Robinson <me@j4y.co>
2016-03-04 22:06:54 +0000

    Change DATA_DIR to "/media/BACKUP/aruna"

--- resources/vagrant_bootstrap.sh
+++ resources/vagrant_bootstrap.sh
@@ -1,15 +1,15 @@
-DATA_DIR="/media/aruna"
+DATA_DIR="/media/BACKUP/aruna"
 
 sudo locale-gen en_GB.UTF-8
 
 ## SORT OUT DATA DIR PERMS
-sudo mkdir $DATA_DIR
-sudo usermod -aG www-data vagrant;
+sudo mkdir -p $DATA_DIR
+sudo usermod -aG www-data pi;
 sudo addgroup www-data;
 sudo chgrp -Rv www-data $DATA_DIR;
 sudo chmod -Rv g+w $DATA_DIR;
 
-sudo touch /srv/.env
+sudo cp /srv/aruna/env.example /srv/.env
 
 sudo apt-get update \
     && sudo apt-get install -y \
