311e48a18c9566351345187e7d1b3535ac007e21
Jay Robinson <me@j4y.co>
2016-03-05 13:22:42 +0000

    Clean up old tests

--- tests/SystemTest.php
+++ tests/SystemTest.php
@@ -2,10 +2,21 @@
 
 namespace Test;
 
+use GuzzleHttp\Client;
+
 /**
  * Class SystemTest
  * @author John Doe
  */
 class SystemTest extends \PHPUnit_Framework_TestCase
 {
+    public function setUp()
+    {
+        $this->http = new Client(
+            array(
+                'base_uri' => 'http://127.0.0.1',
+                'timeout'  => 2.0
+            )
+        );
+    }
 }
