fbe29a27922c38d5b2da867e9128047e10d4051a
Jay Robinson <me@j4y.co>
2016-03-09 23:01:16 +0000

    Add bootstrap_db and run in bootstrap

---
+++ resources/bootstrap_db.sql
@@ -0,0 +1,1 @@
+CREATE TABLE IF NOT EXISTS posts (id, published, post, PRIMARY KEY (id));
--- resources/vagrant_bootstrap.sh
+++ resources/vagrant_bootstrap.sh
@@ -3,15 +3,6 @@ APP_USER="aruna"
 
 sudo locale-gen en_GB.UTF-8
 
-sudo useradd -G www-data $APP_USER
-## SORT OUT DATA DIR PERMS
-sudo mkdir -p $DATA_DIR
-sudo chown -Rv $APP_USER $DATA_DIR
-sudo chgrp -Rv www-data $DATA_DIR;
-sudo chmod -Rv g+w $DATA_DIR;
-
-sudo cp /srv/aruna/env.example /srv/.env
-
 sudo apt-get update \
     && sudo apt-get install -y \
     nginx \
@@ -25,14 +16,32 @@ sudo apt-get update \
     php5-curl \
     php5-sqlite
 
-## composer
+
+## create data_dir
+sudo useradd -G www-data $APP_USER
+sudo mkdir -p $DATA_DIR
+
+## bootstrap db
+sudo sqlite3 $DATA_DIR/aruna_db.sq3 < /srv/aruna/resources/bootstrap_db.sql
+
+## data dir perms
+sudo chown -Rv $APP_USER $DATA_DIR
+sudo chgrp -Rv www-data $DATA_DIR;
+sudo chmod -Rv g+w $DATA_DIR;
+
+## APP CONFIG
+sudo cp /srv/aruna/env.example /srv/.env
+
+## COMPOSER
 curl -sS https://getcomposer.org/installer | php
 sudo mv composer.phar /usr/local/bin/composer
 
+## NGINX PHP CONFIG
 sudo cp /srv/aruna/resources/nginx.conf /etc/nginx/sites-enabled/default \
     && sudo cp /srv/aruna/resources/php5-fpm.conf /etc/php5/fpm/pool.d/aruna.conf \
     && sudo cp /srv/aruna/resources/supervisord.conf /etc/supervisor/conf.d/
 
+## RESTART SERVICES
 sudo service php5-fpm restart
 sudo service nginx restart
 sudo service supervisor restart
