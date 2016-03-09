be1c5c22c76c43d9bfa870c2ede4f13d02e1c08e
Jay Robinson <me@j4y.co>
2016-03-09 12:15:49 +0000

    Add supervisord to bootstrap

--- resources/vagrant_bootstrap.sh
+++ resources/vagrant_bootstrap.sh
@@ -15,14 +15,22 @@ sudo cp /srv/aruna/env.example /srv/.env
 sudo apt-get update \
     && sudo apt-get install -y \
     nginx \
+    ant \
+    git \
+    supervisor \
     php5-fpm \
     php5-cli \
     php5-imagick \
     php5-curl
 
-sudo cp /srv/aruna/resources/nginx.conf /etc/nginx/sites-enabled/default \
-    && sudo cp /srv/aruna/resources/php5-fpm.conf /etc/php5/fpm/pool.d/aruna.conf
+## composer
+curl -sS https://getcomposer.org/installer | php
+sudo mv composer.phar /usr/local/bin/composer
 
+sudo cp /srv/aruna/resources/nginx.conf /etc/nginx/sites-enabled/default \
+    && sudo cp /srv/aruna/resources/php5-fpm.conf /etc/php5/fpm/pool.d/aruna.conf \
+    #&& sudo cp /srv/aruna/resources/supervisord.conf /etc/supervisor/conf.d/
 
 sudo service php5-fpm restart
 sudo service nginx restart
+sudo service supervisor restart
