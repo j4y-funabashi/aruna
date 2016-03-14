48dc926ea2add57b0d35ef38690716cb0029ed2f
Jay Robinson <me@j4y.co>
2016-03-14 19:58:10 +0000

    WIP start processing mentions

--- app/run.php
+++ app/run.php
@@ -5,6 +5,7 @@ require_once __DIR__ . "/../common.php";
 $app = new Cilex\Application("aruna");
 
 $app['posts_root'] = getenv("ROOT_DIR")."/posts";
+$app['mentions_root'] = getenv("ROOT_DIR")."/webmentions";
 $app['db_file'] = getenv("ROOT_DIR")."/aruna_db.sq3";
 $app['thumbnails_root'] = getenv("ROOT_DIR")."/thumbnails";
 
@@ -49,9 +50,14 @@ $app['process_cache_handler'] = $app->share(function () use ($app) {
     $filesystem = new League\Flysystem\Filesystem($adapter);
     $eventReader = new Aruna\EventReader($filesystem);
 
+    $adapter = new League\Flysystem\Adapter\Local($app['mentions_root']);
+    $filesystem = new League\Flysystem\Filesystem($adapter);
+    $mentionsReader = new Aruna\EventReader($filesystem);
+
     return new Aruna\Handler\ProcessCacheHandler(
         $app['monolog'],
         $eventReader,
+        $mentionsReader,
         $pipeline
     );
 });
--- app/src/Handler/ProcessCacheHandler.php
+++ app/src/Handler/ProcessCacheHandler.php
@@ -11,35 +11,67 @@ class ProcessCacheHandler
     public function __construct(
         $log,
         $eventReader,
+        $mentionsReader,
         $pipeline
     ) {
         $this->log = $log;
         $this->eventReader = $eventReader;
+        $this->mentionsReader = $mentionsReader;
         $this->pipeline = $pipeline;
     }
 
     public function handle()
     {
+
+        //while (true) {
+            $this->processEvents();
+            $this->processMentions();
+            //sleep(60);
+        //}
+    }
+
+    private function processMentions()
+    {
         $initial_id = 0;
         $rpp = 100;
 
-        while (true) {
-            $events = $this->eventReader->listFromId($initial_id, $rpp);
-            foreach ($events as $event) {
-                $this->log->debug("Processing ".$event['uid']);
-                try {
-                    $event = $this->pipeline->process($event);
-                } catch (\Exception $e) {
-                    $m = sprintf(
-                        "Could not process %s [%s]",
-                        $event['uid'],
-                        $e->getMessage()
-                    );
-                    $this->log->critical($m);
+        $mentions = $this->mentionsReader->listFromId($initial_id, $rpp);
+
+        foreach ($mentions as $mention) {
+
+            if (false !== stripos($mention['target'], "http://j4y.co/")) {
+
+                $http = new \GuzzleHttp\Client();
+                $result = $http->get(
+                    $mention['source']
+                );
+                $source_body = trim($result->getBody());
+                if (false !== stripos($source_body, $mention['target'])) {
+                    //$mf = \Mf2\fetch($mention['source']);
+                    var_dump($source_body);
                 }
             }
+        }
+    }
 
-            sleep(60);
+    private function processEvents()
+    {
+        $initial_id = 0;
+        $rpp = 100;
+
+        $events = $this->eventReader->listFromId($initial_id, $rpp);
+        foreach ($events as $event) {
+            $this->log->debug("Processing ".$event['uid']);
+            try {
+                $event = $this->pipeline->process($event);
+            } catch (\Exception $e) {
+                $m = sprintf(
+                    "Could not process %s [%s]",
+                    $event['uid'],
+                    $e->getMessage()
+                );
+                $this->log->critical($m);
+            }
         }
     }
 }
