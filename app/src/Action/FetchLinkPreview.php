4d64896e9625664f5a30f9e236f6e17dba01e530
Jay Robinson <me@j4y.co>
2016-03-17 14:01:09 +0000

    FetchLinkPreview now works for like-of and bookmark-of

--- app/src/Action/FetchLinkPreview.php
+++ app/src/Action/FetchLinkPreview.php
@@ -11,32 +11,73 @@ class FetchLinkPreview
 
     public function __construct(
         $log,
-        $linkPreview
+        $linkPreview,
+        $eventStoreWriter
     ) {
         $this->log = $log;
         $this->linkPreview = $linkPreview;
+        $this->eventStoreWriter = $eventStoreWriter;
     }
 
     public function __invoke($event)
     {
-        if (false === isset($event['bookmark-of'])) {
-            return $event;
-        }
+        $valid_keys = [
+            'bookmark-of',
+            'like-of'
+        ];
 
-        $event['link_preview'] = $this->getLinkPreview($event['bookmark-of']);
+        foreach ($valid_keys as $key) {
+            $event = $this->getLinkPreview($event, $key);
+        }
 
         return $event;
     }
 
-    private function getLinkPreview($link_url)
+    private function getLinkPreview($event, $key)
+    {
+        if (false === isset($event[$key])) {
+            return $event;
+        }
+
+        $out_file = "link_preview/".sha1($event[$key]).".json";
+        if ($this->eventStoreWriter->exists($out_file)) {
+            $link_preview = $this->eventStoreWriter->readContents($out_file);
+            $event['link_preview'] = $link_preview;
+            return $event;
+        }
+
+        $link_preview = $this->fetchLinkPreview($event[$key]);
+        $event['link_preview'] = $link_preview;
+
+        $this->eventStoreWriter->save(
+            $out_file,
+            json_encode($link_preview)
+        );
+
+        return $event;
+    }
+
+    private function fetchLinkPreview($link_url)
     {
         $out = [];
-        $parsed = $this->linkPreview->setUrl($link_url)
-            ->getParsed();
-        foreach ($parsed as $parserName => $link) {
-            $out['url'] = $link->getRealUrl();
-            $out['title'] = $link->getTitle();
-            $out['image'] = $link->getImage();
+        try {
+            $parsed = $this->linkPreview->setUrl($link_url)
+                ->getParsed();
+            foreach ($parsed as $parserName => $link) {
+                $out = [
+                    'url' => $link->getRealUrl(),
+                    'title' => $link->getTitle(),
+                    'image' => $link->getImage(),
+                    'description' => $link->getDescription()
+                ];
+            }
+        } catch (\Exception $e) {
+            $out = [
+                'url' => $link_url,
+                'title' => $link_url,
+                'image' => '',
+                'description' => ''
+            ];
         }
 
         return $out;
