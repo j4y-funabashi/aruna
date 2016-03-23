bd763c9ff7543c6c6c74626254403f406b0927e0
Jay Robinson <me@j4y.co>
2016-03-23 08:19:35 +0000

    tidy

--- tests/unit/MicropubControllerTest.php
+++ tests/unit/MicropubControllerTest.php
@@ -19,6 +19,7 @@ class MicropubControllerTest extends UnitTest
         $this->handler = $this->prophesize("\Aruna\CreateEntryHandler");
         $this->token = $this->prophesize("\Aruna\AccessToken");
         $this->urlGenerator = $this->prophesize("\Symfony\Component\Routing\Generator\UrlGenerator");
+
         $this->SUT = new MicropubController(
             $this->log,
             $this->handler->reveal(),
@@ -32,13 +33,14 @@ class MicropubControllerTest extends UnitTest
      */
     public function it_returns_unauthorized_response_if_token_throws_exception()
     {
+        $error_message = "test";
         $this->token->getTokenFromAuthCode(Argument::cetera())
-            ->willThrow(new \Exception("blah"));
+            ->willThrow(new \Exception($error_message));
 
         $request = new Request();
         $response = $this->SUT->createPost($request);
 
-        $this->assertEquals("blah", $response->getContent());
+        $this->assertEquals($error_message, $response->getContent());
         $this->assertEquals(401, $response->getStatusCode());
     }
 }
