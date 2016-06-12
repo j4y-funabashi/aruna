eed78f235db3fe302678797752a08706daa2e053
Jay Robinson <me@j4y.co>
2016-06-12 23:40:42 +0100

    logging

--- app/src/SendWebmention.php
+++ app/src/SendWebmention.php
@@ -21,19 +21,30 @@ class SendWebmention
     {
         foreach ($this->findUrls->__invoke(print_r($event, true)) as $url) {
             $this->log->info("Finding webmention endpoint [".$url."]");
-            $result = $this->http->request("GET", $url);
+            try {
+                $result = $this->http->request("GET", $url);
+            } catch (\Exception $e) {
+                $m = "Failed to GET ".$url." ".$e->getMessage;
+                $this->log->error($m);
+            }
             $mention_endpoint = $this->discoverEndpoint->__invoke($url, $result, "webmention");
 
             $source_url = "http://j4y.co/p/".$event['uid'];
             $form_params = array("source" => $source_url, "target" => $url);
 
             if ($mention_endpoint != "") {
-                $this->log->info("no endpoint found [".$url."]");
-                $response = $this->http->request(
-                    "POST",
-                    $mention_endpoint,
-                    ["form_params" => $form_params]
-                );
+                $this->log->info("sending mention to [".$mention_endpoint."]");
+
+                try {
+                    $response = $this->http->request(
+                        "POST",
+                        $mention_endpoint,
+                        ["form_params" => $form_params]
+                    );
+                } catch (\Exception $e) {
+                    $m = "Failed to POST to ".$mention_endpoint." ".$e->getMessage;
+                    $this->log->error($m);
+                }
             }
         }
         return $event;
