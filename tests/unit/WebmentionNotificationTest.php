a1f0143b6372196317e8e9ebc48f1fc9fddb178f
Jay Robinson <me@j4y.co>
2016-07-06 14:22:38 +0100

    Add test for generic mentions to WebmentionNotificationTest

--- tests/unit/WebmentionNotificationTest.php
+++ tests/unit/WebmentionNotificationTest.php
@@ -11,6 +11,20 @@ class WebmentionNotificationTest extends UnitTest
     /**
      * @test
      */
+    public function it_prints_message_for_generic_mentions()
+    {
+        $SUT = new WebmentionNotification();
+        $mention = new PostViewModel(json_decode($this->loadJsonFixture("post_note"), true));
+        $post = new PostViewModel(json_decode($this->loadJsonFixture("post_photo"), true));
+
+        $result = $SUT->build($post, $mention);
+        $expected = 'Joe Bloggs linked to your photo "Go Ape, Newcastle" [http://j4y.co/p/20160525153645_5745b87d52719][http://j4y.co/p/20160525153645_5745b87d52719]';
+        $this->assertEquals($expected, $result);
+    }
+
+    /**
+     * @test
+     */
     public function it_prints_message_for_likes()
     {
         $SUT = new WebmentionNotification();
