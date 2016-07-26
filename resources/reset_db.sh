0e981b19f73c230a72503b96e1caa66a8336218a
Jay Robinson <me@j4y.co>
2016-07-26 20:05:21 +0100

    rm all files in DATEDIR before each system test

--- resources/reset_db.sh
+++ resources/reset_db.sh
@@ -2,4 +2,5 @@
 
 DATA_DIR="/media/jayr/aruna"
 
-sudo sqlite3 $DATA_DIR/aruna_db.sq3 < /srv/aruna/resources/bootstrap_db.sql
+rm -rfv $DATA_DIR/*
+sqlite3 $DATA_DIR/aruna_db.sq3 < /srv/aruna/resources/bootstrap_db.sql
