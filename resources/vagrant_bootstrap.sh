b7540b86801ce6f05fdf8b1a8509aaec418230b1
Jay Robinson <me@j4y.co>
2016-08-24 21:49:16 +0100

    update version of docker-compose for vagrant

--- resources/vagrant_bootstrap.sh
+++ resources/vagrant_bootstrap.sh
@@ -11,7 +11,7 @@ install_docker () {
         apt-transport-https \
         ca-certificates
 
-    curl -fsSL https://github.com/docker/compose/releases/download/1.5.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+    curl -fsSL https://github.com/docker/compose/releases/download/1.8.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
     chmod +x /usr/local/bin/docker-compose
     usermod -aG docker vagrant
 }
