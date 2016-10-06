7d99ed20120862180bb2dc088e2ebbf0e5dfc66b
Jay Robinson <me@j4y.co>
2016-10-06 17:27:40 +0100

    Add system test it_creates_a_post_with_file_when_token_is_valid

--- tests/system/CreatePostTest.php
+++ tests/system/CreatePostTest.php
@@ -4,6 +4,9 @@ namespace Test;
 
 use Aruna\Micropub\CreatePostCommand;
 use Aruna\Response\Unauthorized;
+use Aruna\Response\OK;
+use Aruna\Micropub\UploadedFile;
+use Aruna\Micropub\NewPost;
 
 class CreatePostTest extends SystemTest
 {
@@ -25,5 +28,31 @@ class CreatePostTest extends SystemTest
         $expected = sprintf("Invalid access token [%s]", $access_token);
         $this->assertEquals($expected, $result->get("message"));
         $this->assertInstanceOf(Unauthorized::class, $result);
+    }
+
+    /**
+     * @test
+     */
+    public function it_creates_a_post_with_file_when_token_is_valid()
+    {
+        $fake_token = $this->prophesize("\Aruna\Micropub\VerifyAccessToken");
+        $this->app['access_token'] = $fake_token->reveal();
+        $SUT = $this->app['create_post.handler'];
+
+        $command = new CreatePostCommand(
+            $post = ["test" => 1],
+            $files = [
+                new UploadedFile(
+                    __DIR__."/test.jpg",
+                    "jpg",
+                    true,
+                    true
+                )
+            ],
+            $access_token = "Bearer xxx"
+        );
+        $result = $SUT->handle($command);
+        $this->assertInstanceOf(OK::class, $result);
+        $this->assertInstanceOf(NewPost::class, $result->get("items")[0]);
     }
 }
