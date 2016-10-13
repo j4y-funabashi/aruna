a1b9d61c39418768162969c23c99873ec4260676
Jay Robinson <me@j4y.co>
2016-10-13 16:50:36 +0100

    Add DeletePost action

---
+++ app/src/Micropub/DeletePost.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Aruna\Micropub;
+
+class DeletePost
+{
+    public function __construct(
+        $postsRepository
+    ) {
+        $this->postsRepository = $postsRepository;
+    }
+
+    public function __invoke($event)
+    {
+        $this->postsRepository->delete(basename($event["url"]));
+    }
+}
