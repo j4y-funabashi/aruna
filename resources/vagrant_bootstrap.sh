6493e57d3699c04171a7d9a8547f7fd5aadaa060
Jay Robinson <me@j4y.co>
2016-11-16 14:55:20 +0000

    Change vagrant box back to trusty

--- resources/vagrant_bootstrap.sh
+++ resources/vagrant_bootstrap.sh
@@ -5,7 +5,7 @@ install_docker () {
     locale-gen en_GB.UTF-8
     sudo apt-get update -q \
         && sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D \
-        && echo 'deb https://apt.dockerproject.org/repo ubuntu-xenial main' | sudo tee /etc/apt/sources.list.d/docker.list \
+        && echo 'deb https://apt.dockerproject.org/repo ubuntu-trusty main' | sudo tee /etc/apt/sources.list.d/docker.list \
         && sudo apt-get update -q \
         && sudo apt-get install -q -y \
         docker-engine \
