25b87b57fd445334c92c76fad6b506bb2a928bef
Jay Robinson <me@j4y.co>
2016-11-23 12:17:35 +0000

    Update ConvertDataHandler to output event style json

--- app/src/ConvertDataHandler.php
+++ app/src/ConvertDataHandler.php
@@ -22,9 +22,12 @@ class ConvertDataHandler
         foreach ($posts as $post) {
             $out_path = "events/".$post["basename"];
             try {
+                if ($this->dataStore->exists($out_path)) {
+                    $this->dataStore->delete($out_path);
+                }
                 $this->dataStore->save(
                     $out_path,
-                    $this->convertPostData($post)
+                    $this->convertPostToEvent($post)
                 );
             } catch (\League\Flysystem\FileExistsException $e) {
                 $m = sprintf("Skipping s3 write: Event exists %s", $out_path);
@@ -33,18 +36,38 @@ class ConvertDataHandler
         }
     }
 
-    private function convertPostData($post)
+    private function convertPostToEvent($post)
     {
         $post = json_decode(
             $this->dataStore->readContents($post["path"]),
             true
         );
+        return json_encode(
+            [
+                "eventType" => $this->getEventType($post),
+                "eventVersion" => $post["uid"],
+                "eventID" => $post["uid"],
+                "eventData" => $this->convertPostData($post)
+            ]
+        );
+    }
+
+    private function getEventType($post)
+    {
+        if (isset($post["action"]) && $post["action"] == "delete") {
+            return "PostDeleted";
+        }
+        return "PostCreated";
+    }
+
+    private function convertPostData($post)
+    {
         $post = $this->rmAccessToken($post);
         $post = $this->replaceFilesArray($post);
         $post = $this->convertHtoType($post);
         $post = $this->stringsToArrays($post);
         $post = $this->convertToMf2($post);
-        return json_encode($post);
+        return $post;
     }
 
     private function rmAccessToken($post)
