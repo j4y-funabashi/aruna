3bffe8a8d8b83e4b3ae8d53fd4e55bbb1de01a7c
Jay Robinson <me@j4y.co>
2017-06-03 16:21:28 +0100

    Add redirect options to SendWebmention http

--- app/src/Publish/SendWebmention.php
+++ app/src/Publish/SendWebmention.php
@@ -24,7 +24,20 @@ class SendWebmention
         foreach ($this->findUrls->__invoke($post->toString()) as $url) {
 
             try {
-                $result = $this->http->request("GET", $url);
+                $result = $this->http->request(
+                    "GET",
+                    $url,
+                    [
+                        'connect_timeout' => 5,
+                        'allow_redirects' => [
+                            'max'             => 10,
+                            'strict'          => false,
+                            'referer'         => false,
+                            'protocols'       => ['http', 'https'],
+                            'track_redirects' => false
+                        ]
+                    ]
+                );
             } catch (\Exception $e) {
                 $m = "Failed to GET ".$url." ".$e->getMessage();
                 $this->log->error($m);
@@ -32,33 +45,47 @@ class SendWebmention
             }
 
             $mention_endpoint = $this->discoverEndpoint->__invoke($url, $result, "webmention");
-
             if ($mention_endpoint === "") {
                 continue;
             }
 
-            $source_url = $post->url();
-            $form_params = array("source" => $source_url, "target" => $url);
-
-            $this->log->info("sending mention to [".$mention_endpoint."]", $form_params);
-
             try {
-                $response = $this->http->request(
-                    "POST",
-                    $mention_endpoint,
-                    ["form_params" => $form_params, 'http_errors' => false]
+                $this->sendWebmention(
+                    $post->url(),
+                    $url,
+                    $mention_endpoint
                 );
             } catch (\Exception $e) {
                 $m = "Failed to send mention to ".$mention_endpoint." ".$e->getMessage();
                 $this->log->error($m);
                 continue;
             }
-
-            $form_params["response_status_code"] = $response->getStatusCode();
-            $form_params["response_location"] = $response->getHeader("Location");
-
-            $this->log->info("Got response: ", $form_params);
         }
         return $post;
+    }
+
+    private function sendWebmention($source, $target, $mention_endpoint)
+    {
+        $form_params = array("source" => $source, "target" => $target);
+        $this->log->info("sending mention to [".$mention_endpoint."]", $form_params);
+        $response = $this->http->request(
+            "POST",
+            $mention_endpoint,
+            [
+                "form_params" => $form_params,
+                'http_errors' => false,
+                'connect_timeout' => 5,
+                'allow_redirects' => [
+                    'max'             => 10,
+                    'strict'          => false,
+                    'referer'         => false,
+                    'protocols'       => ['http', 'https'],
+                    'track_redirects' => false
+                ]
+            ]
+        );
+        $form_params["response_status_code"] = $response->getStatusCode();
+        $form_params["response_location"] = $response->getHeader("Location");
+        $this->log->info("Got response: ", $form_params);
     }
 }
