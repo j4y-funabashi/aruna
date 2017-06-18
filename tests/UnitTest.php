cffb49fe547007cabc809d4ffe2694dcd8cf0355
Jay Robinson <me@j4y.co>
2017-06-18 18:21:59 +0100

    Add UnitTest helpers

--- tests/UnitTest.php
+++ tests/UnitTest.php
@@ -2,6 +2,12 @@
 
 namespace Test;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use Monolog\Logger;
+use Monolog\Handler\TestHandler;
+
 /**
  * Class UnitTest
  * @author John Doe
@@ -12,5 +18,26 @@ class UnitTest extends \PHPUnit_Framework_TestCase
     protected function loadJsonFixture($file)
     {
         return file_get_contents(__DIR__."/fixtures/".$file.".json");
+    }
+
+    protected function createHttpClient($responses)
+    {
+        $mock = new MockHandler($responses);
+        $handler = HandlerStack::create($mock);
+        return new Client(['handler' => $handler]);
+    }
+
+    protected function createEventStore()
+    {
+        $adapter = new \League\Flysystem\Memory\MemoryAdapter();
+        $filesystem = new \League\Flysystem\Filesystem($adapter);
+        return new \Aruna\EventStore($filesystem);
+    }
+
+    protected function createLogger()
+    {
+        $logger = new Logger('test');
+        $logger->pushHandler(new TestHandler());
+        return $logger;
     }
 }
